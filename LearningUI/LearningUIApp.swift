61df92ad7fe76d5cf10197ab9bac94f48b41c50f
prabhuIOSDev <prabhuiosdeveloper@gmail.com>
2025-08-04 22:49:31 +0530

    Initial Commit

---
+++ LearningUI/LearningUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningUIApp.swift
+//  LearningUI
+//
+//  Created by Prabhu Devar on 04/08/25.
+//
+
+import SwiftUI
+
+@main
+struct LearningUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LearningUITests/LearningUITests.swift
@@ -0,0 +1,17 @@
+//
+//  LearningUITests.swift
+//  LearningUITests
+//
+//  Created by Prabhu Devar on 04/08/25.
+//
+
+import Testing
+@testable import LearningUI
+
+struct LearningUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
